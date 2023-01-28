b112cad9fada0ad781befb6b4fc60e87eede4815
Michael_Gichamu <michaelmatere606@gmail.com>
2023-01-28 06:13:40 -0800

    print

--- print_char.c
+++ print_char.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * print_char - prints a char
@@ -32,18 +33,5 @@ int print_string(va_list s)
 		_putchar(str[count]);
 	}
 	return (count);
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-
 }
 
